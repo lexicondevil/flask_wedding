1fdfd8bdaf650a79ce6c2ec1378e8ae6f79ce413
Rob <xbadbmx@gmail.com>
2016-04-23 20:05:52 -0700

    ok no ssl 4 nao

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask_sslify import SSLify
 
 app = Flask(__name__)
-sslify = SSLify(app)
+#sslify = SSLify(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
