d065ea2cbae7da5531056972974c4b0ab482d41e
Rob <xbadbmx@gmail.com>
2016-04-25 19:42:12 -0700

    maybe v.01?

--- app/views.py
+++ app/views.py
@@ -4,12 +4,8 @@ from .models import Rsvps
 from .forms import RsvpForm
 import time
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html')
-
-@app.route('/rsvp/', methods=['GET', 'POST'])
-def rsvp():
   form = RsvpForm(csrf_enabled=True)
   if form.validate_on_submit():
       newrsvp = Rsvps(
@@ -20,14 +16,6 @@ def rsvp():
       db.session.add(newrsvp)
       db.session.commit()
       time.sleep(3)
-      print 'Saving your information...'
-
-  #flash('this is a print for {}'.format(rsvp))
-      flash('We\'ve got {} and your +{} all rsvpeed'.format(form.name.data, form.guests.data))
-      flash('Where specifically: Our backyard, 5040 N Oberlin PDX, 97203')
-      flash('When specifically: July 30th, 4pm -> 9pm')
-      flash('Check your {} email address for event information + updates'.format(form.email.data))
-      flash('Questions? Feel free to email/text us!')
-      return render_template('rsvpeed.html', form=form)
-  else:
-      return render_template('rsvp.html', form=form)
+      flash('Saving your information...')
+      flash('THANKS FOR COMING TO OUR SHINDIG!')
+  return render_template('index.html', form=form)
